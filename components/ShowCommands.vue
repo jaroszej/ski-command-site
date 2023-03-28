1daed736677a8da7e5f88a75b629c496e51ed833
Zasserr <zasserrj@gmail.com>
2023-03-28 15:43:12 -0400

    Initial commit

---
+++ components/ShowCommands.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Skimmands</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Command</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="command in commands" :key="command.key">
+          <td>{{ command.key }}</td>
+          <td>{{ command.value }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { getCommands } from '../database/db'
+
+export default {
+  name: 'SkiestiCommands',
+  data () {
+    return {
+      commands: {}
+    }
+  },
+  async created () {
+    this.commands = await getCommands()
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="components">
+    <img
+      src="../assets/img/rockyCool.png"
+      alt="rockyCool twitch emote"
+      width="200"
+    >
+    <ShowCommands />
+  </div>
+</template>
+
+<script>
+import ShowCommands from '../components/ShowCommands.vue'
+
+export default {
+  name: 'IndexPage',
+  components: { ShowCommands }
+}
+</script>
